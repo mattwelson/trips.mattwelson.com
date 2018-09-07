b77d417972ade388f010306a7aba7f97c76000f9
mattwelson <matthew.welson@gmail.com>
2018-09-07 13:52:28 +1200

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,14 @@
 // Prevents animations running in the first 2 seconds of page load
 // Previously the framerate was dropping a lot
 exports.onInitialClientRender = () => {
+  console.log('render')
   setTimeout(
-    () =>
+    () => {
+      console.log('remove preload')
       document
         .querySelectorAll('.preload')
-        .forEach(n => n.classList.remove('preload')),
+        .forEach(n => n.classList.remove('preload'))
+    },
     2000
   )
 }
