d0f7c236c87737cb20154ac065304f69c6d71f60
mattwelson <matthew.welson@gmail.com>
2019-03-27 10:33:19 +1300

    Alice changed her instagram

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -2,5 +2,5 @@ export default {
   github: 'https://github.com/mattwelson/trips.mattwelson.com',
   twitter: 'https://twitter.com/mattwelson',
   instagram: 'https://www.instagram.com/m.welson/',
-  instagramAlice: 'https://www.instagram.com/_alicemilne_/'
+  instagramAlice: 'https://www.instagram.com/alice.adventuring/'
 }
